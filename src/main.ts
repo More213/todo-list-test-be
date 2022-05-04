e77fbb4765df9f86e6b43ddf8631a880c63ece50
More213 <esenin.teen@gmail.com>
2022-05-05 00:38:54 +0300

    make it better

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import express from 'express';
+import path from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(express.static(path.join(__dirname, 'build')));
+
   app.enableCors()
   await app.listen(process.env.PORT || 3000, () => {
     console.log(`Server started on port ${process.env.PORT || 3000}`);
