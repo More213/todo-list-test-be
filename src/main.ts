2cde67d23519eed89ced0d3617f664d8c135642a
More213 <esenin.teen@gmail.com>
2022-05-06 01:37:45 +0300

    make it better

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import path from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const app = await NestFactory.create(AppModule);
   // app.use(express.static(path.join(__dirname, 'build/todo-list-test-fe')));
 
   // app.enableCors()
-  await app.listen(3000, () => {
+  await app.listen(process.env.PORT || 3000, () => {
     console.log(`Server started on port ${process.env.PORT || 3000}`);
   });
 }
