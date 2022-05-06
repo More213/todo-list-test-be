7647de6314498fa17a2325eb28daa387ced1daa7
More213 <esenin.teen@gmail.com>
2022-05-06 21:16:13 +0300

    make it better

--- build/app.module.js
+++ build/app.module.js
@@ -23,7 +23,7 @@ AppModule = __decorate([
                 useNewUrlParser: true
             }),
             serve_static_1.ServeStaticModule.forRoot({
-                rootPath: (0, path_1.join)(__dirname, '..', 'build/todo-list-test-fe'),
+                rootPath: (0, path_1.join)(__dirname, 'build', 'todo-list-test-fe'),
             }),
             category_module_1.CategoryModule,
         ],
